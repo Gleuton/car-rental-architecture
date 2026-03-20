7173c3516d909d8af4264ba98140fefaaf3e0e61
Gleuton Dutra <gleuton.dutra@gmail.com>
2026-03-20 16:39:22 -0300

    refactor: componentização do home

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,10 @@ import { createApp } from 'vue';
 const app = createApp({});
 
 import LoginComponent from './components/Login.vue';
+import HomeComponent from './components/Home.vue';
+
 app.component('login-component', LoginComponent);
+app.component('home-component', HomeComponent);
 
 // Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
 //     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Bem-vindo</div>
+
+                    <div class="card-body">
+                        Você está logado!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
