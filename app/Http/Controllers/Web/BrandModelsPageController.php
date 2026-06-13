0faf1addb779f25719f3f46ed404e3e649ffd4cd
Gleuton Dutra <gleuton.dutra@gmail.com>
2026-06-13 16:50:28 -0300

    feat(models): estrutura básica para tela de modelos

---
+++ app/Http/Controllers/Web/BrandModelsPageController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Support\Renderable;
+
+class BrandModelsPageController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(): Renderable
+    {
+        return view('car_models.index');
+    }
+}
---
+++ resources/views/car_models/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <car-model-component></car-model-component>
+@endsection
+
