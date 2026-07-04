749c1ef5d94317a9f5e5c6ff8484c213e1ba0985
Gleuton Dutra <gleuton.dutra@gmail.com>
2026-07-04 17:38:08 -0300

    refactor(brand): replace duplicated primitives with shared implementations
    useBrandList and useBrandDelete become thin wrappers around the shared
    composables, preserving their public API. Brand.vue switches to the
    shared Pagination, SearchInput and DeleteModal components.
    Removes PaginationBrand, SearchBrand and BrandDeleteModal.

--- resources/js/modules/brand/composables/useBrandDelete.js
+++ resources/js/modules/brand/composables/useBrandDelete.js
@@ -1,51 +1,10 @@
-import {reactive} from 'vue';
-import {deleteBrandByUuid, getBrandDetailsByUuid} from '@modules/brand/services/brandApi.js';
-import {Modal} from "bootstrap";
+import { useEntityDelete } from '@shared/composables/useEntityDelete.js';
+import { deleteBrandByUuid, getBrandDetailsByUuid } from '@modules/brand/services/brandApi.js';
 
-export function useBrandDelete({onSuccess} = {}) {
-    const runOnSuccess = onSuccess ?? (() => {});
-    const deleteInfo = reactive({
-        uuid: null,
-        name: '',
+export function useBrandDelete({ onSuccess } = {}) {
+    return useEntityDelete({
+        fetchDetailsFn: getBrandDetailsByUuid,
+        deleteFn: deleteBrandByUuid,
+        onSuccess,
     });
-
-    function resetDeleteInfo() {
-        deleteInfo.uuid = null;
-        deleteInfo.name = '';
-    }
-
-    function closeModal() {
-        const modal = document.querySelector('.modal.show');
-        Modal.getInstance(modal)?.hide();
-    }
-
-    function getDeleteInfo(uuid) {
-        return getBrandDetailsByUuid(uuid)
-            .then((response) => {
-                deleteInfo.uuid = response.data.data.uuid;
-                deleteInfo.name = response.data.data.name;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
-
-    function deleteSubmit(uuid) {
-        return deleteBrandByUuid(uuid)
-            .then(() => {
-                runOnSuccess();
-                closeModal();
-                resetDeleteInfo();
-            })
-            .catch((error) => {
-                console.error(error);
-            })
-    }
-
-    return {
-        deleteInfo,
-        resetDeleteInfo,
-        deleteSubmit,
-        getDeleteInfo,
-    };
 }
